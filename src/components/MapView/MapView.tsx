6ead25384efa34a87521cbdd5579213c0bf5df78
nafiulnipu <mnipu2@uic.edu>
2022-05-25 15:52:13 -0500

    a simple canvas added

--- src/components/MapView/MapView.tsx
+++ src/components/MapView/MapView.tsx
@@ -4,18 +4,18 @@ import { useEffect, useRef } from 'react'
 import './MapView.css';
 
 export const MapView = () => {
-    // const canvas = useRef<HTMLCanvasElement>(null!);
+    const canvas = useRef<HTMLCanvasElement>(null!);
     const div = useRef<HTMLDivElement>(null!);
+    // console.log(canvas.current.getContext('webgl2'))
+    console.log(canvas.current)
+    // let gl = document.querySelector(canvas.current)
+    useEffect(()=> {
+        console.log(canvas.current.clientHeight)
+    }, [canvas])
 
-    // useEffect(() => {
-    //     console.log('use')
-    //     const canvasElement = canvas.current;
-    //     canvasElement.width = window.innerWidth / 1.25;
-    //     canvasElement.height = window.innerHeight - 20;
-    // }, [])
     return(
         <Col md={11}>
-            {/* <canvas ref={canvas} style={{border:"1px solid #000000"}}></canvas> */}
+            <canvas className='mapCanvas' ref={canvas} style={{border:"1px solid #000000"}}></canvas>
             <div ref={div} className={'mapPlaceHolderDiv'}>Map</div>
         </Col>
     )
