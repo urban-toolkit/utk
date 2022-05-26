7ef1446e2daeba913763b6853a5a81e289ba83d9
nafiulnipu <mnipu2@uic.edu>
2022-05-26 14:23:15 -0500

    placement div removed from mapview

--- src/components/MapView/MapView.tsx
+++ src/components/MapView/MapView.tsx
@@ -32,8 +32,6 @@ var viewMatrix: Iterable<number>;
 
 export const MapView = () => {
     const canvas = useRef<HTMLCanvasElement>(null!);
-    const div = useRef<HTMLDivElement>(null!);
-
 
     function updateModelMatrix() {
 
@@ -129,7 +127,6 @@ export const MapView = () => {
     return(
         <Col md={11}>
             <canvas className='mapCanvas' ref={canvas} style={{border:"1px solid #000000"}}></canvas>
-            <div ref={div} className={'mapPlaceHolderDiv'}>Map</div>
         </Col>
     )
 }
