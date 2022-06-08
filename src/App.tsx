96a65853a89d5880053736821ac239190a4a2e6e
nafiulnipu <mnipu2@uic.edu>
2022-06-08 16:36:30 -0500

    responsive svgs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 // css file
 import './App.css';
 
@@ -43,6 +43,8 @@ const barData = [
 ];
 
 function App() {
+  // size to maintain responsiveness
+  const size = useWindowResize();
   // example iris data for scatter
   const scatterData = useData()
   // example heatmap data 
@@ -88,8 +90,8 @@ function App() {
       // BOOLEAN - whether to show vis or not
         disp = {barChartView}
         data={barData}
-        width={width}
-        height={height}
+        width={size.width}
+        height={size.height}
         margin={margin}
         scaleOffset={scaleOffset}
         yScaleOffset={yScaleOffset}
@@ -102,8 +104,8 @@ function App() {
         // BOOLEAN - whether to show vis or not
         disp = {scatterPlotView}
         data={scatterData}
-        width={width}
-        height={height}
+        width={size.width}
+        height={size.height}
         margin={margin}
         scaleOffset={scaleOffset}
         yScaleOffset={yScaleOffset}
@@ -116,8 +118,8 @@ function App() {
         // BOOLEAN - whether to show vis or not
         disp = {heatmapView}
         data = {heatData}
-        width={width}
-        height={height}
+        width={size.width}
+        height={size.height}
         margin={margin}
         scaleOffset={scaleOffset}
         yScaleOffset={yScaleOffset}
@@ -128,6 +130,35 @@ function App() {
       </Row>
     </Container>
   );
+}
+
+
+// making responsive
+function useWindowResize(){
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth / 3,
+    height: window.innerHeight / 3,
+  });
+
+  useEffect(() => {
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth / 3,
+        height: window.innerHeight / 3,
+      });
+    }
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+  return windowSize;
 }
 
 export default App;
