c0d506305792c0c302d4a93b913fe33413597fd7
nafiulnipu <mnipu2@uic.edu>
2022-06-16 13:29:11 -0500

    update mapView

--- src/components/MapView/MapView.tsx
+++ src/components/MapView/MapView.tsx
@@ -41,8 +41,9 @@ export const MapViewer = ({dataToView}:mapViewDataProps) => {
         // Environment.setEnvironment({backend: 'http://127.0.0.1:3000', dataFolder:'src/data/data_format'});
         const url = `https://gist.githubusercontent.com/nafiul-nipu/1be0e281b5e7c1415bb239297660a998/raw/78196c1143f00bf29e91e1ee542ca6308bd07267/park_slope_no_buildings.json`;
         // const url = `https://gist.githubusercontent.com/nafiul-nipu/62a79715f9200212bc35227a0d4100e9/raw/0a13d94866e863356ed915dc57bf556c47da7dd5/index.json`
-
+        console.log(url)
         DataLoader.getJsonData(url).then(data => {
+          console.log(data)
             app.run(data);
           });
 
