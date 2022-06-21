8dd22abee91412813f6af24eb8b2b16fa57df594
nafiulnipu <mnipu2@uic.edu>
2022-06-21 15:58:43 -0500

    env added

--- src/components/MapView/MapView.tsx
+++ src/components/MapView/MapView.tsx
@@ -3,7 +3,7 @@ import {Col} from 'react-bootstrap'
 
 import React, {useEffect, useRef } from 'react'
 // urbantkmap.js
-import { MapView, DataLoader } from '../../urbantk-map/ts/dist/urbantkmap';
+import {Environment, MapView, DataLoader } from '../../urbantk-map/ts/dist/urbantkmap';
 // jquery
 import $ from 'jquery';
 // mapview css
@@ -39,9 +39,10 @@ export const MapViewer = ({dataToView, divWidth}:mapViewDataProps) => {
         
         // console.log(window.innerHeight)
         // Data fromat example
-        // Environment.setEnvironment({backend: 'http://127.0.0.1:3000', dataFolder:'src/data/data_format'});
-        const url = `https://gist.githubusercontent.com/nafiul-nipu/1be0e281b5e7c1415bb239297660a998/raw/78196c1143f00bf29e91e1ee542ca6308bd07267/park_slope_no_buildings.json`;
-        // const url = `https://gist.githubusercontent.com/nafiul-nipu/62a79715f9200212bc35227a0d4100e9/raw/0a13d94866e863356ed915dc57bf556c47da7dd5/index.json`
+        // Environment.setEnvironment({backend: 'http://127.0.0.1:3000', dataFolder:'src/data/refs_format'});
+        // const url = `https://gist.githubusercontent.com/nafiul-nipu/1be0e281b5e7c1415bb239297660a998/raw/78196c1143f00bf29e91e1ee542ca6308bd07267/park_slope_no_buildings.json`;
+        const url = `https://gist.githubusercontent.com/nafiul-nipu/62a79715f9200212bc35227a0d4100e9/raw/0a13d94866e863356ed915dc57bf556c47da7dd5/index.json`
+        // const url = `${Environment.backend}/${Environment.dataFolder}/index.json`;
         console.log(url)
         DataLoader.getJsonData(url).then(data => {
           console.log(data)
