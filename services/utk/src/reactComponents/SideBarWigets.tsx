f262e6330cc20df6b9f11b1cbdc2dcdd31b5e31f
Gustavo Moreira <m.muller.gustavo@gmail.com>
2023-07-05 22:39:10 -0700

    Adding sidebard

--- services/utk/src/reactComponents/SideBarWigets.tsx
+++ services/utk/src/reactComponents/SideBarWigets.tsx
@@ -3,6 +3,10 @@ import { ComponentIdentifier, WidgetType } from "../constants";
 import { IComponentPosition, IGenericWidget, IView } from "../interfaces";
 import { ToggleKnotsWidget } from './ToggleKnotsWidget';
 import { SearchWidget } from './SearchWidget';
+import {Row} from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLayerGroup } from '@fortawesome/free-solid-svg-icons'
+import * as d3 from "d3";
 
 type SideBarWidgetsProps = {
     x: number,
@@ -17,14 +21,30 @@ type SideBarWidgetsProps = {
 
 export const SideBarWidgets = ({x, y, mapWidth, mapHeight, layersIds, knotVisibility, inputBarId, viewObjs}:SideBarWidgetsProps) =>{
 
+    const handleClickLayers = (e: any) => {
+      console.log("clicked");
+
+      if(d3.select("#toggle_knot_widget").style("display") == "block"){
+          d3.select("#toggle_knot_widget").style("display", "none");
+      }else{
+        d3.select("#toggle_knot_widget").style("display", "block");
+      }
+
+
+    }
+
     return (
         <React.Fragment>
-        <div style={{backgroundColor: "#EAEAEA", height: "100%", width: "100%"}}>
+        <div style={{backgroundColor: "white", height: "100%", width: "100px", position: "absolute", left: 0, top: 0}}>
+          <Row>
+            <FontAwesomeIcon icon={faLayerGroup} onClick={handleClickLayers} />
+          </Row>
+
           {
             viewObjs.map((component, index) => {
               if(component.type == WidgetType.TOGGLE_KNOT){
                 return <React.Fragment key={"toggle_knot_"+index}>
-                  <div className='component' style={{position: "absolute", left: 0, top: 0, width: 200, height: 200}}>
+                  <div className='component' id="toggle_knot_widget" style={{position: "absolute", left: 110, top: 0, width: 200, height: 200, display: "none"}}>
                     <ToggleKnotsWidget
                       obj = {component.obj}
                       listLayers = {layersIds}
@@ -36,19 +56,21 @@ export const SideBarWidgets = ({x, y, mapWidth, mapHeight, layersIds, knotVisibi
                     />
                   </div>
                 </React.Fragment>
-              }else if(component.type == WidgetType.SEARCH){
-                return <React.Fragment key={"search_"+index}>
-                  <div style={{borderRadius: "8px 8px 8px 8px"}} style={{position: "absolute", left: x, top: y}}>
-                    <SearchWidget 
-                      obj = {component.obj}
-                      viewId = {"search_"+index}
-                      inputId = {inputBarId}
-                      title = {component.title}
-                      subtitle = {component.subtitle}
-                    />
-                  </div>
-                </React.Fragment>
               }
+              
+              // else if(component.type == WidgetType.SEARCH){
+              //   return <React.Fragment key={"search_"+index}>
+              //     <div style={{borderRadius: "8px 8px 8px 8px"}} style={{position: "absolute", left: x, top: y}}>
+              //       <SearchWidget 
+              //         obj = {component.obj}
+              //         viewId = {"search_"+index}
+              //         inputId = {inputBarId}
+              //         title = {component.title}
+              //         subtitle = {component.subtitle}
+              //       />
+              //     </div>
+              //   </React.Fragment>
+              // }
             })
           }
         </div>
