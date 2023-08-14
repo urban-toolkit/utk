d84ca2b505ee92d678740672613abaa340b44584
Gustavo Moreira <m.muller.gustavo@gmail.com>
2023-08-14 18:04:48 -0500

    Fixing jsoneditor

--- services/utk/src/reactComponents/VanillaJSONEditor.tsx
+++ services/utk/src/reactComponents/VanillaJSONEditor.tsx
@@ -1,35 +1,77 @@
-import { JSONEditor } from "vanilla-jsoneditor";
+import JSONEditor from "jsoneditor";
+import '../../node_modules/jsoneditor/dist/jsoneditor.min.css';
 import { useEffect, useRef } from "react";
 
-export default function SvelteJSONEditor(props: any) {
+// declaring the types of the props
+type GrammarEditorProps = {
+  content: any,
+  readOnly: boolean,
+  onChange: any,
+  mode: string,
+  identation: number
+}
+
+export default function GrammarEditor({
+    content,
+    readOnly,
+    onChange,
+    mode,
+    identation
+  }: GrammarEditorProps) {
   const refContainer = useRef(null);
   const refEditor = useRef(null);
 
   useEffect(() => {
-    // @ts-ignore
-    refEditor.current = new JSONEditor({
-      // @ts-ignore
-      target: refContainer.current,
-      props: {}
-    });
+    // // @ts-ignore
+    // refEditor.current = new JSONEditor({
+    //   // @ts-ignore
+    //   target: refContainer.current,
+    //   props: {}
+    // });
 
-    return () => {
-      // destroy editor
-      if (refEditor.current) {
-        // @ts-ignore
-        refEditor.current.destroy();
-        refEditor.current = null;
-      }
-    };
+    // return () => {
+    //   // destroy editor
+    //   if (refEditor.current) {
+    //     // @ts-ignore
+    //     refEditor.current.destroy();
+    //     refEditor.current = null;
+    //   }
+    // };
+
+    console.log("content", content);
+
+    // create the editor
+    const container = document.getElementById("div-jsoneditor")
+    const options = {}
+    refEditor.current = new JSONEditor(container as HTMLElement, options)
+
+    // set json
+    const initialJson = {
+        "Array": [1, 2, 3],
+        "Boolean": true,
+        "Null": null,
+        "Number": 123,
+        "Object": {"a": "b", "c": "d"},
+        "String": "Hello World"
+    }
+    refEditor.current.set(content)
+
   }, []);
 
-  // update props
   useEffect(() => {
     if (refEditor.current) {
       // @ts-ignore
-      refEditor.current.updateProps(props);
+      refEditor.current.set(content);
     }
-  }, [props]);
+  }, [content]);
 
-  return <div className="vanilla-jsoneditor-react" ref={refContainer}></div>;
+  // // update props
+  // useEffect(() => {
+  //   if (refEditor.current) {
+  //     // @ts-ignore
+  //     refEditor.current.updateProps(props);
+  //   }
+  // }, [props]);
+
+  return <div id="div-jsoneditor"></div>;
 }
