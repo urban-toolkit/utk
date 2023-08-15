7a44c0c64d583ef0cc71f8ed48fa279d979d0a90
Gustavo Moreira <gmorei3@uic.edu>
2023-08-15 10:15:42 -0500

    Initial changes

--- services/utk/src/reactComponents/VanillaJSONEditor.tsx
+++ services/utk/src/reactComponents/VanillaJSONEditor.tsx
@@ -1,4 +1,4 @@
-import { JSONEditor } from "vanilla-jsoneditor";
+import JSONEditor from "jsoneditor";
 import { useEffect, useRef } from "react";
 
 export default function SvelteJSONEditor(props: any) {
@@ -6,30 +6,48 @@ export default function SvelteJSONEditor(props: any) {
   const refEditor = useRef(null);
 
   useEffect(() => {
-    // @ts-ignore
-    refEditor.current = new JSONEditor({
-      // @ts-ignore
-      target: refContainer.current,
-      props: {}
-    });
 
-    return () => {
-      // destroy editor
-      if (refEditor.current) {
-        // @ts-ignore
-        refEditor.current.destroy();
-        refEditor.current = null;
-      }
-    };
+    const container = document.getElementById("jsoneditor-div");
+    const options = {}
+    const editor = new JSONEditor(container as HTMLElement, options)
+
+    // set json
+    const initialJson = {
+      "Array": [1, 2, 3],
+      "Boolean": true,
+      "Null": null,
+      "Number": 123,
+      "Object": {"a": "b", "c": "d"},
+      "String": "Hello World"
+    }
+
+    editor.set(initialJson)
+
+    // // @ts-ignore
+    // refEditor.current = new JSONEditor({
+    //   // @ts-ignore
+    //   target: refContainer.current,
+    //   props: {}
+    // });
+
+    // return () => {
+    //   // destroy editor
+    //   if (refEditor.current) {
+    //     // @ts-ignore
+    //     refEditor.current.destroy();
+    //     refEditor.current = null;
+    //   }
+    // };
   }, []);
 
-  // update props
-  useEffect(() => {
-    if (refEditor.current) {
-      // @ts-ignore
-      refEditor.current.updateProps(props);
-    }
-  }, [props]);
+  // // update props
+  // useEffect(() => {
+  //   if (refEditor.current) {
+  //     // @ts-ignore
+  //     refEditor.current.updateProps(props);
+  //   }
+  // }, [props]);
 
-  return <div className="vanilla-jsoneditor-react" ref={refContainer}></div>;
+  // return <div className="vanilla-jsoneditor-react" ref={refContainer}></div>;
+  return <div id="jsoneditor-div" ref={refContainer}></div>;
 }
