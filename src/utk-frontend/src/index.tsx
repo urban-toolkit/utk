b3e7b70194e668b998e157dc07664684f33ef297
Vinícius de Lima Costa <viniciuslc476@gmail.com>
2023-09-20 18:26:39 -0300

    feat:first working test with jupyter notebooks

--- src/utk-frontend/src/index.tsx
+++ src/utk-frontend/src/index.tsx
@@ -1,36 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './App';
-import reportWebVitals from './reportWebVitals';
-// bootstrap css
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Jupyter from './Jupyter';
+import React from "react";
+import ReactDOM from "react-dom";
+import { select } from "d3-selection";
+import "./index.css";
+import App from "./App";
 
-declare global{
-  interface Window {
-    JupyterReact: any;
-  }
-}
+// bootstrap css
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // const root = ReactDOM.createRoot(
 //   document.getElementById('root') as HTMLElement
 // );
 // root.render(
 //   <React.StrictMode>
-//     <App />
+//     <App/>
 //   </React.StrictMode>
 // );
-window.JupyterReact = {
-  init: (selector: string, myData: { bar: any; scatter: any; heat: any; city: any }) => {
-    selector = selector.substring(1);
-    const renderComponent = (<Jupyter {...myData} />)
 
-    ReactDOM.render(renderComponent, document.getElementById(selector));
-  }
+export function renderMap(divName: any) {
+  //   const root = ReactDOM.createRoot(
+  //     document.getElementById("root") as HTMLElement
+  //   );
+  //   root.render(
+  //     <React.StrictMode>
+  //       <App />
+  //     </React.StrictMode>
+  //   );
+
+  ReactDOM.render(<App />, select(divName).node());
 }
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/utk-frontend/webpack.config.js
@@ -0,0 +1,72 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const config = {
+  entry: ["./src/index.tsx"],
+  output: {
+    path: __dirname + "/build",
+    filename: "utk.js",
+    library: "utk",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/react"],
+        },
+      },
+      {
+        test: /\.(ts|d.ts|tsx)$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.less$/,
+        use: ["style-loader", "css-loader", "less-loader"],
+      },
+      {
+        test: /\.(jpg|png|svg)$/,
+        loader: "url-loader",
+        options: {
+          limit: Infinity, // everything
+        },
+      },
+      {
+        test: /\.(glsl|vs|fs)$/,
+        loader: "raw-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
+
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".d.ts", ".css"],
+  },
+  devServer: {
+    hot: false,
+    devMiddleware: {
+      writeToDisk: true,
+    },
+    static: {
+      directory: path.join(__dirname, "build"),
+    },
+    port: 3000,
+  },
+  mode: "development",
+};
+module.exports = config;
